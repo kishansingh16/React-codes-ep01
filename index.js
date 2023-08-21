b46ea175b5b7a01f7a2c5d45bad41f8a39ab8110
Juhi <juhi1617@gmail.com>
2023-08-21 20:51:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const parent= React.createElement(
+    "div",
+    {id:"parent"},[
+        React.createElement(
+            "div",
+            {id:"child"},
+            [React.createElement( "h1",{},"Im an H1 tag"),
+            React.createElement( "h2",{},"Im an H2 tag")
+        ]),
+        React.createElement(
+            "div",
+            {id:"child"},
+            [React.createElement( "h1",{},"Im an H1 tag"),
+            React.createElement( "h2",{},"Im an H2 tag")
+        ])
+    ]
+    
+    )
+
+// const heading=React.createElement("h1",{id:"heading"},"Hello world from React");
+
+console.log(parent); //object
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
